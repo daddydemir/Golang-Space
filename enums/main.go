672188439802951ad21337717564b0ef3a8b23c8
daddydemir <mehmetcakmaktasi47@gmail.com>
2022-08-17 10:50:08 +0300

    enumların kullanımını öğrendim.

---
+++ enums/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+const (
+	GET    string = "GET"
+	POST   string = "POST"
+	PUT    string = "PUT"
+	DELETE string = "DELETE"
+	PATCH  string = "PATCH"
+)
+
+func main() {
+	fmt.Println(GET)
+}
+
+// enumlar bu şekilde kullanılıyor.
