5e75e4dfd76c8ba861df4b902acd696a3c8a6be5
daddydemir <42716195+daddydemir@users.noreply.github.com>
2024-01-07 17:36:23 +0300

    add: graceful shutdown added for kafka consumer.

--- gracefulShutdown/main.go
+++ gracefulShutdown/main.go
@@ -5,23 +5,32 @@ import (
 	"errors"
 	"log"
 	"net/http"
+	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 	"time"
+
+	"github.com/segmentio/kafka-go"
 )
+
+var shutdownCh chan struct{}
+var wg sync.WaitGroup
 
 func main() {
 
+	shutdownCh = make(chan struct{})
+
 	http.HandleFunc("/buyTheProduct", buyTheProduct)
+	http.HandleFunc("/consume", startConsume)
 
 	srv := http.Server{
 		Addr:    ":8080",
 		Handler: nil,
 	}
 
-	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
-
-	defer stop()
+	signalCh := make(chan os.Signal, 1)
+	signal.Notify(signalCh, syscall.SIGINT, syscall.SIGTERM)
 
 	go func() {
 		if err := srv.ListenAndServe(); err != nil && !errors.Is(err, http.ErrServerClosed) {
@@ -29,10 +38,61 @@ func main() {
 		}
 	}()
 
-	<-ctx.Done()
-	log.Println("got interruption signal.")
-	if err := srv.Shutdown(context.TODO()); err != nil {
-		log.Printf("server shutdown returned an err: %v\n", err)
+	select {
+	case sig := <-signalCh:
+		log.Printf("Received signal: %v. Shutting down...", sig)
+		close(shutdownCh)
+		wg.Wait()
+		if err := srv.Shutdown(context.Background()); err != nil {
+			log.Printf("Server shutdown returned an error: %v\n", err)
+		}
+	}
+
+}
+
+func startConsume(w http.ResponseWriter, r *http.Request) {
+
+	log.Println("Consumer is started.")
+	wg.Add(1)
+	go Consume()
+
+}
+
+func Consume() {
+
+	defer wg.Done()
+
+	reader := kafka.NewReader(kafka.ReaderConfig{
+		Brokers:     []string{"localhost:9092"},
+		Topic:       "gracefulShutdown",
+		GroupID:     "group-1",
+		StartOffset: kafka.FirstOffset,
+	})
+
+	for {
+		select {
+		case <-shutdownCh:
+			log.Println("kafka consumer closing.")
+			reader.Close()
+			return
+
+		default:
+			m, err := reader.FetchMessage(context.Background())
+			if err != nil {
+				log.Println("read error: ", err)
+			}
+
+			log.Printf("message: %v ", string(m.Value))
+			time.Sleep(2 * time.Second)
+
+			err = reader.CommitMessages(context.Background(), m)
+			if err != nil {
+				log.Println("commit error: ", err)
+			} else {
+				log.Println("commit successfully : ", string(m.Value))
+			}
+		}
+
 	}
 
 }
